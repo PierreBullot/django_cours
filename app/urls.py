bfdb4ee6f406f677935aebbcace24c4d7aca357f
Pierre Bullot <pierre-bullot@hotmail.fr>
2025-10-16 15:36:22 +0200

    data directory added, images inside listed and linked to

--- app/urls.py
+++ app/urls.py
@@ -21,4 +21,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('book/<str:path>', views.book),
+    path('cover/<str:path>', views.cover)
 ] + debug_toolbar_urls()
--- app/views.py
+++ app/views.py
@@ -1,9 +1,29 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 from django.shortcuts import render
+from django.conf import settings
+import os
+
+images_directory = f"{settings.BASE_DIR}/data"
+
 
 def index(request):
+    images_list = os.listdir(images_directory)
     breadcrumb = [
-        ("Home", "/")
+        ("Home", "")
     ]
-    context = {'name': 'toto', "breadcrumb": breadcrumb}
+    context = {"breadcrumb": breadcrumb, "book_url": "/book/", "images": images_list}
     return render(request, "index.html", context)
+
+
+def book(request, path):
+    breadcrumb = [
+        ("Home", "/"),
+        (path, "")
+    ]
+
+    context = {"breadcrumb": breadcrumb, "cover_url": f"/cover/{path}"}
+    return render(request, "details.html", context)
+
+
+def cover(request, path):
+    return FileResponse(open(f"{images_directory}/{path}", "rb"))
